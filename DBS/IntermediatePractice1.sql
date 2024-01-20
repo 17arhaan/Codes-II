924edea5ca88f8476f01d9db73fa9e14c69d3f0e
Arhaan <17arhaan@gmail.com>
2024-01-20 19:14:01 +0530

    syncc

--- DBS/IntermediatePractice1.sql
+++ DBS/IntermediatePractice1.sql
@@ -1,8 +1,35 @@
 use UNIVERSITY;
 
-select *
-from student join takes on student.ID = takes.ID;
+-- select * from student;
+-- select * from takes;
 
-select *
-from student,takes
-where student.ID = takes.Id;
+-- select *
+-- from student join takes on student.ID = takes.ID;
+
+-- select *
+-- from student,takes
+-- where student.ID = takes.ID;
+
+-- select student.ID as ID , name , dept_name , tot_cred , sec_id , semester , year ,grade
+-- from student , takes
+-- where student.ID = takes.ID;
+
+-- select *
+-- from student natural left outer join takes;
+
+-- select *
+-- from student natural right outer join takes;
+
+-- select *
+-- from ( select *
+--     from student
+--     where dept_name = 'Comp. Sci' )
+--     natural full outer join
+--     ( select * 
+--         from takes
+--         where semester = 'Spring '
+-- );
+
+select * 
+from student left outer join takes on TRUE
+where student.ID = takes.ID;
