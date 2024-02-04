fb43f010e56c7ff103dbb5964db5d1f4fe57b360
Arhaan <17arhaan@gmail.com>
2024-02-05 00:36:25 +0530

    dbs mp

---
+++ AI/TwoStacksQueue.py
@@ -0,0 +1,59 @@
+class TwoStacksQueue:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+    
+    def enqueue(self,item):
+        
+
+def main():
+    ch = 0                            
+
+    print("\n____ MENU ____")
+    print("1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\n")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+# def enqueue(st1, st2, ele):
+#     if not st1:
+#         st2.append(ele)
+#     else:
+#         st1.append(ele)
+#     return
+
+
+# def dequeue(st1, st2):
+#     if not st1:
+#         for i in range(len(st2)):
+#             st1.append(st2.pop())
+#         ans = st1.pop()
+#         for i in range(len(st1)):
+#             st2.append(st1.pop())
+#         return ans
+#     else:
+#         for i in range(len(st1)):
+#             st2.append(st1.pop())
+#         ans = st2.pop()
+#         for i in range(len(st2)):
+#             st1.append(st2.pop())
+#         return ans
+
+
+# if __name__ == '__main__':
+#     a = 0
+#     st1, st2 = [], []
+#     while a != 3:
+#         a = int(input('enter 1 to enqueue 2 to dequeue 3 to exit'))
+#         if a == 1:
+#             b = int(input('enter value'))
+#             enqueue(st1, st2, b)
+#         if a == 2:
+#             print(dequeue(st1, st2))
