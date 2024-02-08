c660faa599d378722242d80355f74fad088fa21d
Arhaan <17arhaan@gmail.com>
2024-02-09 00:03:23 +0530

    selection sort

--- DAA/selectionsort.c
+++ DAA/selectionsort.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
 
-//swapping elements
-
 void swap(int *x , int *y){
     int temp = *x;
     *x = *y;
     *y = temp;
 }
-
-//main sorting function
 
 void selectionsort (int arr[] , int n){
     
@@ -16,21 +12,35 @@ void selectionsort (int arr[] , int n){
     int j;
     int min_pos;
 
-//finding minimum element
-
     for ( i=0; i<n-1; i++){
         min_pos = i;
         for ( j= i+1 ;j<n ; j++){
             if( arr[j] < arr[min_pos] )
             min_pos = j;
-
-//swapping with the minimum element
-
-            if(min_pos != i)
-            swap(&arr[min_pos],&arr[i])
         }
+        if(min_pos != i)
+        swap(&arr[min_pos],&arr[i]);
     }
 }
-//display sorted array
 
-void display(int arr[] , int size)
+void display(int arr[] , int size){
+    for(int i=0 ; i < size ; i++){
+        printf("%d",arr[i]);
+    }
+}
+
+int main(){
+    int arr[99];
+    int size;
+    printf("Enter the size of Array: ");
+    scanf("%d",&size);
+    for(int i = 0 ; i<size ; i++){
+        printf("Enter Element {%d}: ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    // int n = sizeof(arr);
+    selectionsort(arr,size);
+    printf("Sorted Array ---> \n");
+    display(arr,size);
+    return 0;
+}
