4cd4446e326cb9face46ccfad6e3439a1818f4f7
Arhaan <17arhaan@gmail.com>
2024-02-13 00:05:34 +0530

    with statement

--- DBS/Intermediate.sql
+++ DBS/Intermediate.sql
@@ -1,4 +1,40 @@
 use UNIVERSITY;
+use UNIVERSITY;
+
+select * from student;
+select * from takes;
+
+select *
+from student join takes on student.ID = takes.ID;
+
+select *
+from student,takes
+where student.ID = takes.ID;
+
+select student.ID as ID , name , dept_name , tot_cred , sec_id , semester , year ,grade
+from student , takes
+where student.ID = takes.ID;
+
+select *
+from student natural left outer join takes;
+
+select *
+from student natural right outer join takes;
+
+select *
+from ( select *
+    from student
+    where dept_name = 'Comp. Sci' )
+    natural full outer join
+    ( select * 
+        from takes
+        where semester = 'Spring '
+);
+
+select * 
+from student left outer join takes on TRUE
+where student.ID = takes.ID;
+
 --1
 select course_id from takes where semester = 'fall' and year = 2009
 UNION
